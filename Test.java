245a6ae96fbfd810bf53e8f751585fe4c642d12e
MuchoE <scienceplex@gmail.com>
2019-04-18 13:01:18 -0600

    classwork

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test{
+    public static void main(String[] args){
+        double a = Double.parseDouble("123");
+        a += 100;
+        System.out.println(a);
+    }
+}
---
+++ Value.java
@@ -0,0 +1,7 @@
+public class Value{
+
+    public Value(){
+
+    }
+    
+}
